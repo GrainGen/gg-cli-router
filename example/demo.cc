f49c29047925527a9ed2a0c5e0f0267dc1453e69
ljf12825linux <ljf12825@graingen.com>
2026-07-05 22:32:27 +0800

    Feat[example/demo.cc]: init commit

---
+++ example/demo.cc
@@ -0,0 +1,27 @@
+#include "clirouter.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void do_fetch(const std::vector<std::string>& args) {
+    if (args.size() < 2) return;
+    std::cout << "[f2l Fetch] Platform: " << args[0] << ", ID: " << args[1] << "\n";
+}
+
+void do_judge(const std::vector<std::string>& args) {
+    std::cout << "[f2l Judge] Running local tests...\n";
+}
+
+int main(int argc, char* argv[]) {
+    CLIRouter router;
+
+    router.reg_cmd("fetch", do_fetch);
+    router.reg_cmd("judge", do_judge);
+    router.reg_cmd("version", [](const auto& args) {
+            std::cout << "f2l version 0.1 (Codename: Dawu)\n";
+            });
+
+    router.dispatch(argc, argv);
+
+    return 0;
+}
